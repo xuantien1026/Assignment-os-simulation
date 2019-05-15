2b95484c88e42e0576ee048476eb31150ab8989b
xuantien <xuantien1026@gmail.com>
2019-05-15 23:36:05 +0700

    Implement get_proc

--- src/sched.c
+++ src/sched.c
@@ -24,6 +24,17 @@ struct pcb_t * get_proc(void) {
 	 * [ready_queue] and return the highest priority one.
 	 * Remember to use lock to protect the queue.
 	 * */
+	pthread_mutex_lock(&queue_lock);
+	if (!queue_empty()){
+		if(empty(ready_queue)){ // Copy run_queue back to ready_queue
+			for (int i = 0; i < run_queue->size; ++i)
+				ready_queue->proc[i] = run_queue->proc[i];
+			ready_queue->size = run_queue->size;
+			run_queue->size = 0;
+		}
+		proc = dequeue(ready_queue);
+	}
+	pthread_mutex_unlock(&queue_lock);
 	return proc;
 }
 
