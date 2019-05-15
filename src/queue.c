6d93982168211b4210bdc278099ebb54cc36be14
xuantien <xuantien1026@gmail.com>
2019-05-15 23:12:37 +0700

    Implement enqueue and dequeue

--- src/queue.c
+++ src/queue.c
@@ -8,12 +8,32 @@ int empty(struct queue_t * q) {
 
 void enqueue(struct queue_t * q, struct pcb_t * proc) {
 	/* TODO: put a new process to queue [q] */	
+	if (q->size < MAX_QUEUE_SIZE){
+		q->proc[q->size] = proc;
+		q->size += 1;
+	}
 }
 
 struct pcb_t * dequeue(struct queue_t * q) {
-	/* TODO: return a pcb whose prioprity is the highest
+	/* TODO: return a pcb whose priority is the highest
 	 * in the queue [q] and remember to remove it from q
 	 * */
-	return NULL;
+	if (q->size == 0) return NULL;
+	int chosen_proc_index = 0;
+	uint32_t max_priority = (q->proc[0])->priority;
+	// Get highest priority process
+	for (int i = 0; i < q->size; ++i){
+		if ((q->proc[i])->priority > max_priority){
+			max_priority = (q->proc[i])->priority;
+			chosen_proc_index = i;
+		}
+	}
+	pcb_t * chosen_pcb = (q->proc[i])
+	// Remove chosen process from the array and move others up.
+	for (int i = chosen_proc_index; i < q->size - 1; ++i)
+		q->proc[i] = q->proc[i+1];
+	q->size -= 1;
+
+	return chosen_pcb;
 }
 
